3a624cea55eb8fd361a112610acf9ca8ef328193
Eduardo González <laloe.90@gmail.com>
2016-11-24 19:02:21 -0600

    c#

--- Institucion/DataAccess/InstitucionDB.cs
+++ Institucion/DataAccess/InstitucionDB.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Institucion.DataAccess
 {
-    class InstitucionDB
+    public class InstitucionDB : DbContext
     {
     }
 }
